5a7e6d83087a856b2c6b6efeccc8ea956e1fd0d9
Doris Zhang <doriszhang912@gmail.com>
2025-08-16 16:29:03 -0400

    configure test file

--- test.js
+++ test.js
@@ -1,17 +1,31 @@
 const axios = require('axios');
 
-const LB_URL = 'http://localhost:8001'; // change this to your load balancer's URL
+const RR_URL = 'http://localhost:8000'; 
+const WRR_URL = 'http://localhost:8001';
 const NUM_REQUESTS = 10; // number of test requests
 
-async function testLB() {
-  for (let i = 0; i < NUM_REQUESTS; i++) {
-    try {
-        const response = await axios.get(LB_URL);
-        console.log(`Request ${i + 1}: ${response.data}`);
-      } catch (error) {
-        console.error(`Request ${i + 1} failed:`, error.message);
-      }
-  }
+async function testLBs() {
+    // test Round Robin
+    console.log('Testing round robin');
+    for (let i = 0; i < NUM_REQUESTS; i++) {
+        try {
+            const response = await axios.get(RR_URL);
+            console.log(`Request ${i + 1}: ${response.data}`);
+        } catch (error) {
+            console.error(`Request ${i + 1} failed:`, error.message);
+        }
+    }
+    
+    // testing Weighted Round Robin
+    console.log('Testing weighted round robin');
+    for (let i = 0; i < NUM_REQUESTS; i++) {
+        try {
+            const response = await axios.get(WRR_URL);
+            console.log(`Request ${i + 1}: ${response.data}`);
+        } catch (error) {
+            console.error(`Request ${i + 1} failed:`, error.message);
+        }
+    }
 }
 
-testLB();
+testLBs();
